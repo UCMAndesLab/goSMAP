0b5b01b9f7e690f23482a683ffead5cf0bba7159
Alex Beltran <abeltran2@ucmerced.edu>
2016-04-08 14:16:23 -0700

    Include tests.

---
+++ connection_test.go
@@ -0,0 +1,69 @@
+package gosMAP
+
+import (
+    "fmt"
+    "github.com/go-ini/ini"
+    "testing"
+)
+
+var server string
+var apiKey string
+var uuid string
+func init(){
+  cfg, err := ini.Load("test.ini")
+  if err != nil{
+    panic(err)
+  }
+
+  server = cfg.Section("MAIN").Key("server").String()
+  apiKey = cfg.Section("MAIN").Key("apiKey").String()
+  uuid   =  cfg.Section("MAIN").Key("uuid").String()
+}
+
+func TestConnection(t *testing.T) {
+    _,err := connect(server, apiKey)
+    if err != nil{
+      t.Error(err.Error())
+    }
+}
+
+func TestDataCollection(t *testing.T){
+    conn,err := connect(server, apiKey)
+    if err == nil{
+      fmt.Printf("%s\n", conn.Url)
+      _, err := conn.data_uuid(uuid, 0, 0, 10)
+
+      if err !=nil {
+        t.Error(err.Error())
+      }
+    }else{
+      t.Error(err.Error())
+    }
+}
+
+func TestQuery(t *testing.T){
+    conn,err := connect(server, apiKey)
+    if err != nil{
+      t.Error(err.Error())
+    }
+    conn.query(fmt.Sprintf("select * where uuid='%s'", uuid))
+}
+
+func TestTags(t *testing.T){
+    conn,err := connect(server, apiKey)
+    if err != nil{
+      t.Error(err.Error())
+    }
+    d := conn.tags(uuid)
+    if (uuid != d[0].Uuid){
+      t.Error("UUID Mismatch")
+    }
+    fmt.Printf("%s\n", string(d[0].Metadata["SourceName"].(string)))
+}
+
+func BenchmarkTagsCache(b *testing.B) {
+    conn,_ := connect(server, apiKey)
+    for i := 0; i < b.N; i++ {
+      conn.tags(uuid)
+    }
+}
