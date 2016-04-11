2ede01dd945fc5e9d83abf43604fb054abfd296e
Alex Beltran <abeltran2@ucmerced.edu>
2016-04-11 14:48:44 -0700

    Move logic out of query since it is used beyond the query system. Need a better organization system.

---
+++ helpers.go
@@ -0,0 +1,53 @@
+package gosMAP
+
+import (
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "io/ioutil"
+)
+
+func pullData(url string) ([]sMAPData, error){
+  d := make([]rawsMAPData,0)
+  r := make([]sMAPData,0)
+  response, err := http.Get(url)
+  if err != nil {
+    return r, err
+  }
+
+  defer response.Body.Close()
+  contents, err := ioutil.ReadAll(response.Body)
+  if err != nil {
+    return r, err
+  }
+
+  err = json.Unmarshal(contents, &d)
+  if err != nil {
+    return r, err
+  }
+
+  r = rawDataToClean(d)
+  return r, nil
+}
+
+func rawDataToClean(dirty []rawsMAPData) []sMAPData{
+  r := make([]sMAPData, len(dirty))
+
+  for i,d := range dirty{
+    r[i].Uuid = d.Uuid
+    r[i].Readings = make([]readPair, len(d.Readings))
+
+    for j,entry := range d.Readings{
+      r[i].Readings[j].value,_ = entry[1].Float64()
+    }
+  }
+  return r;
+}
+func smap_time(t int) string{
+  if t == 0{
+    return ""
+  }else{
+    // smap measures from microseconds since epoch so times it by 1000.
+    return fmt.Sprintf("%d", t*1000);
+  }
+}
--- query.go
+++ query.go
@@ -4,7 +4,6 @@ import (
   "bytes"
   "fmt"
   "net/http"
-  "encoding/json"
   "io/ioutil"
 )
 
@@ -36,49 +35,4 @@ func (conn *sMAPConnection) Data_uuid(uuid string, starttime int, endtime int, l
   url := fmt.Sprintf("%sapi/data/uuid/%s?startime=%s&endtime=%s&limit=%d", conn.Url, uuid, starttime_str, endtime_str, limit)
 
   return pullData(url)
-}
-
-func pullData(url string) ([]sMAPData, error){
-  d := make([]rawsMAPData,0)
-  r := make([]sMAPData,0)
-  response, err := http.Get(url)
-  if err != nil {
-    return r, err
-  }
-
-  defer response.Body.Close()
-  contents, err := ioutil.ReadAll(response.Body)
-  if err != nil {
-    return r, err
-  }
-
-  err = json.Unmarshal(contents, &d)
-  if err != nil {
-    return r, err
-  }
-
-  r = rawDataToClean(d)
-  return r, nil
-}
-
-func rawDataToClean(dirty []rawsMAPData) []sMAPData{
-  r := make([]sMAPData, len(dirty))
-
-  for i,d := range dirty{
-    r[i].Uuid = d.Uuid
-    r[i].Readings = make([]readPair, len(d.Readings))
-
-    for j,entry := range d.Readings{
-      r[i].Readings[j].value,_ = entry[1].Float64()
-    }
-  }
-  return r;
-}
-func smap_time(t int) string{
-  if t == 0{
-    return ""
-  }else{
-    // smap measures from microseconds since epoch so times it by 1000.
-    return fmt.Sprintf("%d", t*1000);
-  }
 }
