98a906660a8cbb452a6b7e14aff84e618297a4d4
Alex Beltran <abeltran2@ucmerced.edu>
2016-04-26 14:09:21 -0700

    Fix data missing due to missing api key

--- prev.go
+++ prev.go
@@ -6,6 +6,6 @@ import (
 
 // Return the last value from given uuid
 func (conn *Connection) Prev(uuid string) ([]Data, error){
-  url := fmt.Sprintf("%sapi/prev/uuid/%s", conn.Url, uuid)
+  url := fmt.Sprintf("%sapi/prev/uuid/%s?key=%s", conn.Url, uuid, conn.APIkey)
   return pullData(url)
 }
